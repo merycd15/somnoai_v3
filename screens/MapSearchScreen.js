8e2840db6448e99bc6a166cba6b57c240dbf8ef8
FerCardozoX <cardozofernando18@gmail.com>
2024-10-27 23:02:04 -0300

    23:01

--- screens/MapSearchScreen.js
+++ screens/MapSearchScreen.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { WebView } from 'react-native-webview';
+//import { WebView } from 'react-native-webview';
 
 const MapSearchScreen = () => {
   return (
     <View style={styles.container}>
-      <WebView
+      {/*<WebView
         source={{ uri: 'https://www.google.com/maps/search/apnea+del+sue%C3%B1o+hospitales/@-34.6077037,-58.4140004,13z/data=!3m1!4b1?entry=ttu&g_ep=EgoyMDI0MTAwMi4xIKXMDSoASAFQAw%3D%3D' }}
         style={{ flex: 1 }}
-      />
+      />*/}
     </View>
   );
 };
